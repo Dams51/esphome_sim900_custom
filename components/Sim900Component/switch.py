147d88cd8dce023a398a405d5887a68784cd44a2
Dams51 <90524688+Dams51@users.noreply.github.com>
2025-03-04 22:06:30 +0100

    Correction du GPIO_Switch

--- components/Sim900Component/switch.py
+++ components/Sim900Component/switch.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import gpio_switch
+from esphome.components.gpio import switch
 
 from . import CONF_SIM900_ID, Sim900Component
 
@@ -10,7 +10,7 @@ CONF_POWER_KEY_PIN = "power_key_pin"
 
 CONFIG_SCHEMA = {
     cv.GenerateID(CONF_SIM900_ID): cv.use_id(Sim900Component),
-    cv.Optional(CONF_POWER_KEY_PIN): gpio_switch.CONFIG_SCHEMA,
+    cv.Optional(CONF_POWER_KEY_PIN): switch.CONFIG_SCHEMA,
 }
 
 
@@ -18,5 +18,5 @@ async def to_code(config):
     sim900_component = await cg.get_variable(config[CONF_SIM900_ID])
 
     if CONF_POWER_KEY_PIN in config:
-        sens = await gpio_switch.new_gpio_switch(config[CONF_POWER_KEY_PIN])
+        sens = await switch.new_switch(config[CONF_POWER_KEY_PIN])
         # cg.add(sim900_component.set_registered_binary_sensor(sens))
