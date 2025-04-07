93a5aba3e9ef984058e0179ab479df78f8d06274
Dams51 <90524688+Dams51@users.noreply.github.com>
2025-04-07 22:40:58 +0200

    Gérer le cas où l'on décracho mais qu'il n'y a pas d'appel

--- components/Sim900Component/sim900.cpp
+++ components/Sim900Component/sim900.cpp
@@ -103,6 +103,9 @@ void Sim900Component::parse_cmd_(std::string message) {
         this->call_state_ = 6;
         // this->call_disconnected_callback_.call();
       }
+      if (this->state_ == STATE_ATA_SENT) {
+        this->state_ == STATE_INIT
+      }
       this->watch_dog_ = 0;
       return; // Next message
     } else if (message.compare(0, 6, "+CMTI:") == 0) {
